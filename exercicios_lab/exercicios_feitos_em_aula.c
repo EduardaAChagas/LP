2f22ba2308cc2dce201cb3ae35a759ff5ecbeea6
EduardaAChagas <adraudech@gmail.com>
2019-09-17 23:33:28 -0300

    terceiro exercicio

--- exercicios_lab/exercicios_feitos_em_aula.c
+++ exercicios_lab/exercicios_feitos_em_aula.c
@@ -7,16 +7,42 @@ int main(){
     scanf("%f",&temp);
     celsius=temp-(32.0*(5.0/9.0));
     printf("Tempreratura em Fahrenheit:%f\nTemperatua em Celsius:%f\n",temp,celsius);
+    getch();
     return 0;
 }
 */
 //2-Dadas as medidas dos catetos de um triangulo retangulo, informe a medida da hipotenusa
-
+/*
 #include <math.h>
 int main(void){
     int catetoA,catetoB,hipotenusa;
     scanf("%d %d",&catetoA,&catetoB);
     hipotenusa=sqrt(pow(catetoA,2)+pow(catetoB,2));
     printf("Hipotenusa: %d\n",hipotenusa);
+    getch();
     return 0;
 }
+*/
+//desenvolver um algoritmo que verifica se um numero eh divisivel por 5 e 3 ao mesmo tempo
+int main(void){
+    int num;
+    scanf("%d",&num);
+    if (num%5==0 && num%3==0) printf("sim");
+    else printf("nao");
+    getch();
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
