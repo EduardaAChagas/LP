2878f319d2eb455822048563d450d88fec5adcdb
Eduarda Maria de Albuquerque Chagas <eduardachagas@ufma.deinf.local>
2019-09-19 10:59:29 -0300

    exercicios monitoria

---
+++ monitoria/LPtoria/Lptoria.c
@@ -0,0 +1,163 @@
+#include <stdio.h>
+/*
+int aaa(x){
+    int i,j;
+    int esq=x,dir=x;long long int fatorial(int x){
+	if (x == 0){
+		return 1.0;
+	}
+	return (x * fatorial(x-1) * 1.0);
+}
+
+
+    for (i=1;i<=x;i++){
+        for (j=1;j<=(2*x-1);j++){
+            if(j<=dir && j>=esq)
+                printf("*");
+            else
+                printf(" ");
+        }
+        esq--;
+        dir++;
+        printf("\n");
+    }
+}
+int main(){int aaa(x){
+    int i,j;
+    int esq=x,dir=x;long long int fatorial(int x){
+	if (x == 0){
+		return 1.0;
+	}
+	return (x * fatorial(x-1) * 1.0);
+}
+
+
+    for (i=1;i<=x;i++){
+        for (j=1;j<=(2*x-1);j++){
+            if(j<=dir && j>=esq)
+                printf("*");
+            else
+                printf(" ");
+        }
+        esq--;
+        dir++;
+        printf("\n");
+    }
+}
+int main(){
+    int x;
+    scanf("%d",&x);
+    aaa(x);
+    return 0;
+}
+    int x;
+    scanf("%d",&x);
+    aaa(x);
+    return 0;
+}
+*/
+
+
+
+
+/*
+
+#include <math.h>
+//calcular o fatorial exponencial de um numero
+long long int fatorial(int x){
+    int i,aux;
+    for (i=1;i==x;i++){
+        aux=pow(x,x-1);
+    }
+    return aux;
+}
+int main(void){
+    int n;
+    scanf("%d",&n);
+    printf("%lld",fatorial(n));
+    return 0;
+}
+*/
+
+
+
+
+
+
+
+/*
+
+int soma(int a, int b, int c){
+    return a+b+c;int aaa(x){
+    int i,j;
+    int esq=x,dir=x;long long int fatorial(int x){
+	if (x == 0){
+		return 1.0;
+	}
+	return (x * fatorial(x-1) * 1.0);
+}
+
+
+    for (i=1;i<=x;i++){
+        for (j=1;j<=(2*x-1);j++){
+            if(j<=dir && j>=esq)
+                printf("*");
+            else
+                printf(" ");
+        }
+        esq--;
+        dir++;
+        printf("\n");
+    }
+}
+int main(){
+    int x;
+    scanf("%d",&x);
+    aaa(x);
+    return 0;
+}
+}
+int main(void){
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    printf("%d",soma);
+    return 0;
+}
+
+
+
+
+char funcao(a){
+    if (a>0) return  'p';
+    else return 'n';
+}
+int main(){
+    int a;
+    scanf("%d",&a);
+    printf("%c",funcao(a));
+}
+*/
+int somaimposto(int a,int b){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
