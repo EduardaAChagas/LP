85d15cf9b440b13deda31963f95e2e66b6317ab3
Eduarda Maria de Albuquerque Chagas <eduardachagas@ufma.deinf.local>
2019-09-30 17:09:15 -0300

    rotacionar vetor

--- exercicios_lab/slide 4/LP_30092019.c
+++ exercicios_lab/slide 4/LP_30092019.c
@@ -4,7 +4,7 @@
 //Escreva um programa em C para deletar um valor de uma certa posição
 //(fornecida como entrada) de um vetor, em seguida, mostre esse vetor.
 /*
-int main(){
+int main(void){
 	int n,i,p=5;
 	int v[p],v2[p-1];
 	for (i=0;i<p;i++){
@@ -26,4 +26,39 @@ int main(){
 	return 0;
 }
 */
+/*
+int main(void){
+	int v[]={1,2,3,4};
+	int tam=sizeof(v)/sizeof(int);
+	int n,i,j,k;
+	printf("rotacionar quantas vezes\n");
+	scanf("%d",n);
+
+	
+	for (i=0;i<n;i++){
+		int primeiro=v[0];
+		for (j=0;j<tam-1;j++){
+			v[j]=v[j+1];	
+		}
+	v[tam-1]=primeiro;		
+	}
+	for (k=0;k<tam;k++){
+		printf("%d",v[k]);
+	}
+	return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 
