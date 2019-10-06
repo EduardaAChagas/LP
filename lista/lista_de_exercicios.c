7d180b80188d323a669a73db4e399be68a64e9c2
Eduarda <adraudech@gmail.com>
2019-10-06 01:03:12 -0300

    lista 1

--- lista/lista_de_exercicios.c
+++ lista/lista_de_exercicios.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-//ler os 4 valores correspondentes aos eixos x e y de dois pontos quaisquer no plano e calcule a distancia entre
+#include <math.h>
+//01.ler os 4 valores correspondentes aos eixos x e y de dois pontos quaisquer no plano e calcule a distancia entre
 //eles,mostrando 4 casas decimais após a vírgula.
+/*
 #include <math.h>
 int main(){
 	float x1,y1,x2,y2,dist;
@@ -9,3 +11,42 @@ int main(){
 	printf("%.4f",dist);
     return 0;
 }
+*/
+//11. dar a raiz de um numero usando recursividade e o metodo de newton
+
+float raiz(float n, float r){
+	if (abs(pow(r,2)-n)<0.001) return r;
+	else {
+		r=(pow(r,2)+n)/(2*r);
+		raiz(n,r);
+	}
+}
+int main(){
+	float n;
+	int r=1;
+	scanf("%f",&n);
+	printf("%.2f",raiz(n,r));
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
