3578e6f70c7649e62da270a9caff7b94200ff472
Eduarda Maria de Albuquerque Chagas <eduardachagas@ufma.deinf.local>
2019-10-23 16:56:38 -0300

    transformando letras minusculas em letras maiusculas com ponteiros

--- exercicios_lab/topic 6/exercicos_lab.c
+++ exercicios_lab/topic 6/exercicos_lab.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+
+void paraMaiuscula(char *string){
+	while(*string!='\0'){
+		if (*string>='a' && *string<='z')
+			*string=*string-('a'-'A');
+		string++;
+	}
+}
 int main(){
-	
+	char string[100];
+	fgets(string,100,stdin);
+	char* ptr;
+	ptr=string;
+	paraMaiuscula(ptr);
+	printf("%s\n",string);
+	getch();
 	return 0;
 }
+
